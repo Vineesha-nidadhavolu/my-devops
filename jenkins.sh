ed3a49b287049581433a9bc432fd43fe5a9fb008
Vineesha-nidadhavolu <vineesha8143@gmail.com>
2024-09-25 15:07:00 +0530

    successfully created file

---
+++ jenkins.sh
@@ -0,0 +1,12 @@
+add new content
+:wq!
+
+
+The exception thrown when trying to access a local variable inside a function or method which has no value assigned to it is UnboundLocalError.
+
+
+:wq!
+
+
+
+
